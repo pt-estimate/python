3c255a26f0dc7c44f12dea734d9f9ac09feab67d
pt-estimate <christopherwright@protonmail.com>
2015-07-28 09:31:23 -0700

    Adding twos_binomial_proportion.py skeleton

---
+++ difference_tests/twos_binomial_proportion.py
@@ -0,0 +1,9 @@
+"""Script to test difference in binomial proportions for two populations"""
+import sys
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
